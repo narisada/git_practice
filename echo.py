29688796161a071e8afe0affa1375629328f821b
narisada <shintaro_narisada@shino.ecei.tohoku.ac.jp>
2015-04-10 19:02:00 +0900

    echo.pyにhelpを実装しました

--- echo.py
+++ echo.py
@@ -1,13 +1,19 @@
-#echo.py
+#!/usr/bin/python 
+# -*- coding: utf-8 -*-
 
 import sys
 
 argvs = sys.argv
 argc  = len(argvs)
+s = ""
 
-if  argc >1:
+
+if argc >1:
     for i in range(1,argc):
-      print argvs[i]
-
-
-
+        if ( argvs[i] == "--help"):
+            print "echoのhelpです",
+            quit()
+    for i in range(1,argc):
+        print argvs[i],
+            
+print ""
